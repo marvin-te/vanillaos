bedf972e0b244b511d46de2f36a6f94ffd0f37c0
Marvin Templin <marvin@tmpl.dev>
2024-09-13 13:31:53 +0000

    fake latest debian stable version 🤞

--- includes.container/deb-pkgs/add-docker-gpg.sh
+++ includes.container/deb-pkgs/add-docker-gpg.sh
@@ -8,6 +8,6 @@ chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  sid stable" | \
+  bookworm stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
